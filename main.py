f3230db054b643cae4dff8a8b8e205af0d2dd770
SANNIHITH <ihm2014004@iiita.ac.in>
2019-02-24 11:56:38 +0530

    Add files via upload
    For clear understanding refer the attached pptx.

---
+++ main.py
@@ -0,0 +1,7 @@
+print hello
+docker run -v ${PWD:/medium-facenet-tutorial} \
+-e PYTHONPATH=${PYTHONPATH:/medium-facenet-tutorial} \
+-it colemurray/medium-facenet-tutorial python3 /medium-facenet-tutorial/medium_facenet_tutorial/preprocess.py \
+--input-dir /medium-facenet-tutorial/data \
+--output-dir /medium-facenet-tutorial/output/intermediate \
+--crop-dim 180
